f3405c3eb8e02c4ec421a3b5843ef3278cb915c5
sankalp srivatava <Sankalp2015415@akgec.ac.in>
2022-06-16 16:27:39 +0530

    setup server

---
+++ app.js
@@ -0,0 +1,109 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send('fdskhfjkasdhkjf');
+});
+
+app.get('/products', (req, res) => {
+    res.status(200).json([
+        {
+            prodName: "djskhfkjladshf",
+            price: 2434353453,
+            age: 18
+        },
+        {
+            prodName: "djskhfkjladshf",
+            price: 2434353453,
+            age: 18
+        },
+        {
+            prodName: "djskhfkjladshf",
+            price: 2434353453,
+            age: 18
+        },
+        {
+            prodName: "djskhfkjladshf",
+            price: 2434353453,
+            age: 18
+        },
+        {
+            prodName: "djskhfkjladshf",
+            price: 2434353453,
+            age: 18
+        },
+        {
+            prodName: "djskhfkjladshf",
+            price: 2434353453,
+            age: 18
+        },
+        {
+            prodName: "djskhfkjladshf",
+            price: 2434353453,
+            age: 18
+        },
+        {
+            prodName: "djskhfkjladshf",
+            price: 2434353453,
+            age: 18
+        },
+        {
+            prodName: "djskhfkjladshf",
+            price: 2434353453,
+            age: 18
+        },
+        {
+            prodName: "djskhfkjladshf",
+            price: 2434353453,
+            age: 18
+        },
+        {
+            prodName: "djskhfkjladshf",
+            price: 2434353453,
+            age: 18
+        },
+        {
+            prodName: "djskhfkjladshf",
+            price: 2434353453,
+            age: 18
+        },
+        {
+            prodName: "djskhfkjladshf",
+            price: 2434353453,
+            age: 18
+        },
+        {
+            prodName: "djskhfkjladshf",
+            price: 2434353453,
+            age: 18
+        },
+        {
+            prodName: "djskhfkjladshf",
+            price: 2434353453,
+            age: 18
+        },
+        {
+            prodName: "djskhfkjladshf",
+            price: 2434353453,
+            age: 18
+        },
+        {
+            prodName: "djskhfkjladshf",
+            price: 2434353453,
+            age: 18
+        },
+        {
+            prodName: "djskhfkjladshf",
+            price: 2434353453,
+            age: 18
+        }
+    ]);
+});
+
+app.listen(80, () => {
+    console.log('server running on 127.0.0.1');
+});
